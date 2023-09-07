db5cfefbd29385e652c084be99263664c84c7c8f
Emydidae <peterjsolis@gmail.com>
2023-09-07 19:03:22 -0400

    Final Commit

---
+++ ERD & Postgres/crowdfunding_db_schema.sql
@@ -0,0 +1,54 @@
+-- drop if existing
+DROP TABLE IF EXISTS campaign;
+DROP TABLE IF EXISTS contacts;
+DROP TABLE IF EXISTS category;
+DROP TABLE IF EXISTS subcategory;
+
+
+-- create tables
+CREATE TABLE contacts (
+	contact_id INT NOT NULL,
+	first_name VARCHAR(20) NOT NULL,
+	last_name VARCHAR(20) NOT NULL,
+	email VARCHAR(70) NOT NULL,
+	PRIMARY KEY (contact_id)
+);
+
+CREATE TABLE category (
+	category_id VARCHAR(5) NOT NULL,
+	category VARCHAR(30) NOT NULL,
+	PRIMARY KEY (category_id)
+);
+
+CREATE TABLE subcategory (
+	subcategory_id VARCHAR(10) NOT NULL,
+	subcategory VARCHAR(30) NOT NULL,
+	PRIMARY KEY (subcategory_id)
+);
+
+CREATE TABLE campaign (
+	cf_id INT NOT NULL,
+	contact_id INT NOT NULL,
+	company_name VARCHAR(40) NOT NULL,
+	description VARCHAR(60) NOT NULL,
+	goal FLOAT NOT NULL,
+	pledged FLOAT NOT NULL,
+	outcome VARCHAR(15) NOT NULL,
+	backers_count INT NOT NULL,
+	country VARCHAR(2) NOT NULL,
+	currency VARCHAR(3) NOT NULL,
+	launch_date DATE NOT NULL,
+	end_date DATE NOT NULL,
+	category_id VARCHAR(5) NOT NULL,
+	subcategory_id VARCHAR(10) NOT NULL,
+	PRIMARY KEY (cf_id),
+	FOREIGN KEY (contact_id) REFERENCES contacts(contact_id),
+	FOREIGN KEY (category_id) REFERENCES category(category_id),
+	FOREIGN KEY (subcategory_id) REFERENCES subcategory(subcategory_id)
+);
+
+-- test select statements
+SELECT * FROM campaign;
+SELECT * FROM contacts;
+SELECT * FROM category;
+SELECT * FROM subcategory;
